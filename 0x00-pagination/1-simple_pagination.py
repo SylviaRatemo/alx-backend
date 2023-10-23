f42823ad639e90197213c5122f5d2eed7f46ddaf
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-23 14:04:19 +0300

    Pagination Task 3

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -42,16 +42,6 @@ class Server:
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Calculate the start and end index for a given page and page size.
-
-    Parameters:
-    - page: Page number (1-indexed).
-    - page_size: Number of items per page.
-
-    Returns:
-    A tuple containing the start and end index for the specified page.
-    """
     if page < 1 or page_size < 1:
         raise ValueError("Both page and page_size should be greater than 0.")
 
