56f24e46a6b480639c802f95700781c1f994f947
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-24 11:12:18 +0300

    Basic Cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Baseic Cacche
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache
+      - caching system
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
