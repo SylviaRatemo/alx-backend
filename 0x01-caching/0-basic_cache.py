81956794b8cc383a70dfafbd70376e554178ce9b
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-25 16:38:14 +0300

    Task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,6 +17,4 @@ class BasicCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
         return self.cache_data.get(key)
