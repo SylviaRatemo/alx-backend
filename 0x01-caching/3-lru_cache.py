3ffdb11957e476b27f34b9fbc1572e52a40778be
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-25 19:56:44 +0300

    Cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -4,27 +4,47 @@
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """ LIFO
       - caching system
     """
     def __init__(self):
         super().__init__()
-        self.last_key = ''
+        self.head, self.tail = '-', '='
+        self.next, self.prev = {}, {}
+        self.handle(self.head, self.tail)
+
+    def handle(self, head, tail):
+        """ LRU algorithm, handle elements """
+        self.next[head], self.prev[tail] = tail, head
+
+    def _remove(self, key):
+        """ LRU algorithm, remove element """
+        self.handle(self.prev[key], self.next[key])
+        del self.prev[key], self.next[key], self.cache_data[key]
+
+    def _add(self, key, item):
+        """ LRU algorithm, add element """
+        self.cache_data[key] = item
+        self.handle(self.prev[self.tail], key)
+        self.handle(key, self.tail)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(self.next[self.head]))
+            self._remove(self.next[self.head])
 
     def put(self, key, item):
         """ Assign to the dictionary """
         if key and item:
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                print('DISCARD: {}'.format(self.last_key))
-                self.cache_data.pop(self.last_key)
-            self.last_key = key
+            if key in self.cache_data:
+                self._remove(key)
+            self._add(key, item)
 
     def get(self, key):
-        """ Return the value"""
+        """ Return the value linked """
         if key is None or self.cache_data.get(key) is None:
             return None
         if key in self.cache_data:
             value = self.cache_data[key]
+            self._remove(key)
+            self._add(key, value)
             return value
